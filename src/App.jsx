195567ff478826d1b80295ca0ede919e10b00976
cameo choquer <cjchoquer@gmail.com>
2023-03-31 15:38:36 +0100

    fixed div duplication - files need refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,33 @@ import './App.css'
 import Stickers from './stickers'
 
 const App = () =>{
+
+  const handlePaste = (event) => {
+     if (event.target.closest('button')|| !navigator.clipboard) {
+      return;
+    }
+
+    navigator.clipboard.readText()
+    .then((text) => {
+        if (text !== '') {
+      const newSticker = document.createElement('div');
+      newSticker.innerText = text;
+      newSticker.style.position = 'absolute';
+      newSticker.style.left = `${event.clientX}px`;
+      newSticker.style.top = `${event.clientY}px`;
+      document.body.appendChild(newSticker);
+      navigator.clipboard.writeText('');
+        }
+    });
+  };
+
+
+   useEffect(() =>{
+    document.body.addEventListener('click', handlePaste );
+    return () => {
+        document.body.removeEventListener('click', handlePaste)
+    }
+   });
   return (
     <div>
       <footer>
@@ -10,7 +37,6 @@ const App = () =>{
         <Stickers stickerEmoji={'🍋'} />
         <Stickers stickerEmoji={'🍊'} />
       </footer>
-     
     </div>
   );
 }
--- src/stickers.jsx
+++ src/stickers.jsx
@@ -2,42 +2,10 @@ import React, { useRef, useEffect } from 'react';
 
 const Stickers = (props) => {
   const stickerRef = useRef(null);
-//   const [currentSticker, setCurrentSticker] = useState('');
 
   const copySticker = () => {
-    // setCurrentSticker(props.stickerEmoji);
     navigator.clipboard.writeText(props.stickerEmoji);
 };
-
-
-  const handlePaste = (event) => {
-     if (event.target.closest('button')|| !navigator.clipboard) {
-      return;
-    }
-
-    navigator.clipboard.readText()
-    .then((text) => {
-        if (text !== '') {
-      const newSticker = document.createElement('div');
-      newSticker.innerText = text;
-      newSticker.style.position = 'absolute';
-      newSticker.style.left = `${event.clientX}px`;
-      newSticker.style.top = `${event.clientY}px`;
-      document.body.appendChild(newSticker);
-      navigator.clipboard.writeText('');
-        }
-      
-    });
-  };
-
-
-   useEffect(() =>{
-    document.addEventListener('click', handlePaste );
-    return () => {
-        document.removeEventListener('click', handlePaste)
-    }
-   });
-
   return (
     <div>
       <button onClick={copySticker}>{props.stickerEmoji}</button>
